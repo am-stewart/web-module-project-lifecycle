ab5caac7d5b3b04fc56ccbd1d9200e8ba45cfba4
Allison Stewart <amstewart@yahoo.com>
2022-01-11 16:01:30 -0500

    set up dom

--- src/App.js
+++ src/App.js
@@ -2,9 +2,37 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
-    return(<div>
-      Github Card
-    </div>);
+    return(
+    <div>
+      <h1>Github Info</h1>
+      <form>
+        <input placeholder={'enter github handle'}/>
+        <button>Search</button>
+      </form>
+      <div>
+        {/*this will be for the Follower.js FROM USER API*/}
+        <img src='https://avatars.githubusercontent.com/u/91036593?v=4'/>
+        <p>Name: Allison Stewart</p>
+        <p>Bio: Full stack web dev student</p>
+        <p>Total Repos: 34</p>
+        <p>Total Followers: 2</p>
+      </div>
+      
+      <div>
+      {/*this will be for the FollowerList.js FROM FOLLOWER API*/}
+      <h3>Followers:</h3>
+      <img width='150' src='https://avatars.githubusercontent.com/u/3699469?v=4'/>
+      <p>Handle: ChristOscar</p>
+      <img width='150' src='https://avatars.githubusercontent.com/u/3699469?v=4'/>
+      <p>Handle: ChristOscar</p>
+      <img width='150' src='https://avatars.githubusercontent.com/u/3699469?v=4'/>
+      <p>Handle: ChristOscar</p>
+      <img width='150' src='https://avatars.githubusercontent.com/u/3699469?v=4'/>
+      <p>Handle: ChristOscar</p>
+      </div>
+
+    </div>
+    );
   }
 }
 
